8b38f9d3b8d53d46dd1d7141f54717a90da55ae8
skyrizer <wafir2002@gmail.com>
2024-02-29 23:42:12 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ import 'bloc/report case/new report case/newCase_bloc.dart';
 import 'bloc/report case/new report case/newCase_state.dart';
 
 // we are at original file
-
+// this is merge
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
